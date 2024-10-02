e42e3ec654aaf8505f23f3d050a8af809c1f1864
Emily Serven <emily@servenupwebsites.net>
2024-10-02 13:02:22 -0400

    Starting groundwork for getting multiple sheets

--- src/utils/ExcelRenderer.tsx
+++ src/utils/ExcelRenderer.tsx
@@ -5,9 +5,14 @@ interface ColumnObj {
     key: number
 }
 
-interface ExcelRendererCallbackResp {
+interface SheetObj {
+    name: string,
     rows: unknown[],
     cols: Array<ColumnObj>[]
+}
+
+interface ExcelRendererCallbackResp {
+    sheets: SheetObj[]
 }
 
 type ExcelRendererCallback = (err: Error | null, resp: ExcelRendererCallbackResp) => void;
@@ -34,9 +39,18 @@ export function ExcelRenderer(file: File, callback: ExcelRendererCallback) {
             const wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
 
             // All sheet names, for debugging
+            console.log('wb', wb);
             console.log('wb.SheetNames', wb.SheetNames);
             // TODO: Make it so sheet names get looped over
             // wil need to loop through and then update the data object to return a few things
+
+            wb.SheetNames.forEach((sheet: string) => {
+                console.log('sheet', sheet);
+                const ws = wb.Sheets[sheet];
+                console.log('ws', ws);
+                console.log('ws test', wb.Sheets['May-June 2021']);
+
+            })
 
             /* Get first worksheet */
             const wsname = wb.SheetNames[0];
@@ -46,7 +60,7 @@ export function ExcelRenderer(file: File, callback: ExcelRendererCallback) {
             const json = XLSX.utils.sheet_to_json(ws, { header: 1 });
             const cols = ws["!ref"] ? make_cols(ws["!ref"]) : { name: 'null', key: 0};
 
-            const data = { rows: json, cols: cols };
+            const data = { name: 'test', rows: json, cols: cols };
 
             resolve(data);
             console.log('data', data);
