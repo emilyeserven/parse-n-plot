9ae7d107f6e215d5de62798d1e8f0c13233469ee
Emily Serven <emily@servenupwebsites.net>
2024-10-02 12:36:20 -0400

    Mostly type annotated except for one stubborn error

--- src/utils/ExcelRenderer.tsx
+++ src/utils/ExcelRenderer.tsx
@@ -1,5 +1,16 @@
 import * as XLSX from 'xlsx';
 
+interface ColumnObj {
+    name: string,
+    key: number
+}
+
+interface ExcelRendererCallbackResp {
+    rows: unknown[],
+    cols: Array<ColumnObj>[]
+}
+
+type ExcelRendererCallback = (err: Error | null, resp: ExcelRendererCallbackResp) => void;
 
 /**
  * Adapting react-excel-renderer to read multiple sheets.
@@ -8,26 +19,37 @@ import * as XLSX from 'xlsx';
  * @constructor
  * @link Original Library - https://github.com/ashishd751/react-excel-renderer/tree/master
  */
-export function ExcelRenderer(file, callback) {
+export function ExcelRenderer(file: File, callback: ExcelRendererCallback) {
+    console.log('file', file);
+    console.log('callback', callback);
     return new Promise(function(resolve, reject) {
-        var reader = new FileReader();
-        var rABS = !!reader.readAsBinaryString;
+        if (reject) {
+            console.log('Rejected: ', reject);
+        }
+        const reader = new FileReader();
+        const rABS = !!reader.readAsBinaryString;
         reader.onload = function(e) {
             /* Parse data */
-            var bstr = e.target.result;
-            var wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
+            const bstr = e.target?.result;
+            const wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
+
+            // All sheet names, for debugging
+            console.log('wb.SheetNames', wb.SheetNames);
+            // TODO: Make it so sheet names get looped over
+            // wil need to loop through and then update the data object to return a few things
 
             /* Get first worksheet */
-            var wsname = wb.SheetNames[0];
-            var ws = wb.Sheets[wsname];
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
 
             /* Convert array of arrays */
-            var json = XLSX.utils.sheet_to_json(ws, { header: 1 });
-            var cols = make_cols(ws["!ref"]);
+            const json = XLSX.utils.sheet_to_json(ws, { header: 1 });
+            const cols = ws["!ref"] ? make_cols(ws["!ref"]) : { name: 'null', key: 0};
 
-            var data = { rows: json, cols: cols };
+            const data = { rows: json, cols: cols };
 
             resolve(data);
+            console.log('data', data);
             return callback(null, data);
         };
         if (file && rABS) reader.readAsBinaryString(file);
@@ -35,10 +57,10 @@ export function ExcelRenderer(file, callback) {
     });
 }
 
-function make_cols(refstr) {
-    var o = [],
+function make_cols(refstr: string) {
+    const o = [],
         C = XLSX.utils.decode_range(refstr).e.c + 1;
-    for (var i = 0; i < C; ++i) {
+    for (let i = 0; i < C; ++i) {
         o[i] = { name: XLSX.utils.encode_col(i), key: i };
     }
     return o;
